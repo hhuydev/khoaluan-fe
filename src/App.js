56910ac87ad054bc92efda48cbde6c0b878f8dd3
OanhNguyenPM13A <nguyenthikieuoanh948@gmail.com>
2021-12-01 22:10:18 +0700

    add change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import LoadingComponent from './component/GlobalSettings/LoadingComponent';
 import GiangVien from './containers/home/giangvien/GiangVien'
 import PhuHuynh from './containers/home/phuhuynh/PhuHuynh'
@@ -13,20 +13,14 @@ export default function App() {
   return (
     <div>
       <BrowserRouter>
-      <LoadingComponent/>
+        <LoadingComponent />
         <Switch>
           <Route exact path="/giangvien" component={GiangVien} />
           <Route exact path="/sinhvien" component={SinhVien} />
           <Route exact path="/phuhuynh" component={PhuHuynh} />
-<<<<<<< HEAD
           <Route exact path="/login" component={Login} />
-          <Route path="/" component={Login} />
-=======
-        
-          <Route exact path="/login" component={Login} /> 
           {/* <Redirect from="/" to="/login" /> */}
-          <Route   path="*" component={PageNotFound} />
->>>>>>> 1522b12a6c9233be2e2d8efc942e63742eafa4a1
+          <Route path="*" component={PageNotFound} />
         </Switch>
       </BrowserRouter>
     </div>
