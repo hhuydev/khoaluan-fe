e3d73ba2ddc6e1e60280e2f7287f5122db47036b
toan.pham <toan.pham@weddingbook.com>
2021-12-05 14:43:36 +0700

    toanpush

---
+++ src/component/giangVien/lopHocItem/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function LopHocItem(props) {
+  console.log(props);
+  const { stt, item } = props;
+  return (
+    <tr>
+      <td>{stt + 1}</td>
+      <td>
+        <NavLink className="nav-link" to={`/giangvien/lophoc/${item.id}`}>{item.id}</NavLink>
+      </td>
+      <td>{item.tenLop}</td>
+      <td>{item.siSo}</td>
+    </tr>
+  );
+}
---
+++ src/containers/home/giangvien/lopHoc/thongTinLopHoc/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ThongTinLopHoc() {
+    return (
+        <div>
+            <h1>THong tin lop hoc</h1>
+        </div>
+    )
+}
